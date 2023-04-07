95c826e22707ad6d3f8be13c458b6c535d7f6134
harlamenkoel <harlamenkoel@gmail.com>
2023-04-08 03:18:23 +0700

    Выполнил

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -3,8 +3,6 @@ package jm.task.core.jdbc;
 import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args)  {
 
